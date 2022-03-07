e35760b2fe8e944a4dccb12d539dbf1f9afb0468
najeesimmons <najee.bryant.simmons@gmail.com>
2022-03-07 16:35:57 -0500

    updated server.js and established connection to MongoDB/ connection successful

--- server.js
+++ server.js
@@ -3,18 +3,35 @@
 // get .env variables
 require("dotenv").config();
 // pull PORT from .env, give default value of 3000
-const { PORT = 3000 } = process.env;
+// pull MONGODB_URL from .env
+const { PORT = 3000, MONGODB_URL } = process.env;
 // import express
 const express = require("express");
 // create application object
 const app = express();
+// import mongoose
+const mongoose = require("mongoose");
 
 
-////////////// ROUTES //////////////
+/////// DATABASE CONNECTION ////////
+/////// Establish Connection ///////
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  // Connection Events
+  mongoose.connection
+    .on("open", () => console.log("Your are connected to mongoose"))
+    .on("close", () => console.log("Your are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+  
+
+
+////////////// ROUTES /////////////
 // create a test route
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-//////////// LISTENER //////////////
+//////////// LISTENER /////////////
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
