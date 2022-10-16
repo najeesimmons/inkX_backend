6fa9938013a82b06fa91869bfaed4888e67037b0
najeesimmons <najee.bryant.simmons@gmail.com>
2022-10-16 14:46:58 -0400

    added user schema

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-// import express
 const express = require("express");
-// create application object
 const app = express();
-// load in .env variables
 require("dotenv").config()
-// import middlware
 const cors = require("cors");
 const morgan = require("morgan");
 
-///////////MiddleWare///////////////
-app.use(cors()); // to prevent cors errors, open access to all origins
-app.use(morgan("dev")); // logging
-app.use(express.json()); // parse json bodies
+app.use(cors()); 
+app.use(morgan("dev"));
+app.use(express.json());
 
-////////////// ROUTES ///////////// * ////////////// ROUTES /////////////
+const PORT = process.env.PORT
 
-/////// create a test route ///////
+// test route
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-
-//////////// LISTENER /////////////
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
