18bc984f9a2f3fc92e259eaa93ae316cfcd0cd1a
najeesimmons <najee.bryant.simmons@gmail.com>
2022-10-16 14:59:10 -0400

    finished user model with singup static method

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -13,3 +15,27 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not stong enough");
+  }
+
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, hash);
+
+  const user = await this.create({ email, password: hash });
+};
+
+module.exports = mongoose.model("User", userSchema);
