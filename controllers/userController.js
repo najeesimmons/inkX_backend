52ab36962e4bb94e14d20e8084fb0e73d8fb7a53
najeesimmons <najee.bryant.simmons@gmail.com>
2022-10-17 12:14:43 -0400

    added login static method to user model, added login static method to controller, sending response back to frontend

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,12 +11,25 @@ const signupUser = async (req, res) => {
     const user = await User.signup(email, password);
     // create jwt
     const token = createToken(user._id);
-    res.status(200).json({ email, token})
+    res.status(200).json({ email, token });
   } catch (error) {
-    res.status(400).json( {error: error.message})
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const { _id } = user;
+    const token = createToken(_id);
+    res.status(200).json({ email, token, _id });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
 
 module.exports = {
   signupUser,
+  loginUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signupUser } = require("../controllers/userController")
+const { signupUser, loginUser } = require("../controllers/userController");
 //need to require "signupuser so that route will have a method attached"
 const router = express.Router();
 
-router.post("/signup", signupUser)
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
 
 module.exports = router;
